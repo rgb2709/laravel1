91323b4f5a5284c3bd290e0c2d8146310b59434d
Renato Barbosa <rgb2709@gmail.com>
2024-12-12 00:16:46 -0300

    primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard')->middleware('verified');
+});
+//Auth::routes(['verify' => true]);
